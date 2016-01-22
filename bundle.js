d871306c6f52b349adc8372b3d6398c9cbe25b46
Tyler Palmer <mr.tylerpalmer@gmail.com>
2016-01-22 00:13:09 -0700

    cleaned up design further. simplified sidebar responsiveness. added form borders. centered sidebar text. cleaned up css rules.

--- bundle.js
+++ bundle.js
@@ -583,96 +583,6 @@ trvlApp.factory('util', function() {
   };
 });
 
-trvlApp.controller('citySearchCtrl', ["$scope", "constants", function($scope, constants) {
-  $scope.test = 'Two way directive pass';
-
-  // set bounds of search to the whole world
-  var bounds = new google.maps.LatLngBounds(
-    new google.maps.LatLng(-90, -180),
-    new google.maps.LatLng(90, 180)
-  );
-
-  // get place search input element (only one on page at a time, because ID)
-  var input = document.getElementById('place-search-input');
-
-  // create options object
-  var options = {
-    bounds: bounds,
-    types: ['(cities)']
-  };
-
-  // new autocomplete object that will actually initialize autocomplete
-  var autocomplete = new google.maps.places.Autocomplete(input, options);
-
-  // function to log place data
-  var getPlaceDetails = function() {
-    // raw place data from autocomplete, returned after city selected
-    var rawPlaceData = autocomplete.getPlace();
-
-    $scope.cityData = {}; // create city data object
-
-    // add to cityData object from raw place data
-    $scope.cityData.geo = getAddressComponents(rawPlaceData.address_components);
-    $scope.cityData.placeString = rawPlaceData.formatted_address;
-    $scope.cityData.placeId = rawPlaceData.place_id;
-    $scope.cityData.geo.lat = rawPlaceData.geometry.location.lat();
-    $scope.cityData.geo.lng = rawPlaceData.geometry.location.lng();
-    $scope.cityData.iconUrl = rawPlaceData.icon;
-    $scope.cityData.mapUrl = rawPlaceData.url;
-    $scope.$apply(); // update scope
-    console.log('Place data pulled from google API');
-  };
-
-  // function to get correct address components from address array
-  var getAddressComponents = function(addCompArr) {
-    var finalComps = {};
-    var len = addCompArr.length;
-    // for all address components in array
-    finalComps.city = addCompArr[0].long_name; // always first
-    finalComps.region = addCompArr[len - 2].long_name; // always second to last
-    finalComps.country = addCompArr[len - 1].long_name; // always last
-
-    return finalComps;
-  };
-
-  // when new place is selected, log results obj of place
-  autocomplete.addListener('place_changed', getPlaceDetails);
-
-}]);
-
-trvlApp.directive('citySearch', function() {
-  return {
-    templateUrl: 'app/directives/citySearch/citySearchTmpl.html',
-    restrict: 'E',
-    scope: {
-      cityChosen: '=',
-      cityData: '='
-    },
-    controller: 'citySearchCtrl'
-  };
-});
-
-trvlApp.controller('menuBarCtrl', ["$scope", "authSvc", function($scope, authSvc) {
-
-  $scope.logout = function() {
-    authSvc.signOut();
-  };
-
-
-}]);
-
-trvlApp.directive('menuBar', function() {
-  return {
-    templateUrl: 'app/directives/menuBar/menuBarTmpl.html',
-    restrict: 'E',
-    scope: {
-      userData: '=',
-      currData: '='
-    },
-    controller: 'menuBarCtrl'
-  };
-});
-
 trvlApp.controller('loginCtrl', ["constants", "util", "$scope", "$state", "authSvc", function(constants, util, $scope, $state, authSvc) {
 
   $scope.newuser = false; // default view: login, not new user reg
@@ -806,3 +716,93 @@ trvlApp.controller('tripCtrl', ["$scope", "$stateParams", "currAuth", "dataOps",
   console.log($scope); // for monitoring/ debugging
 
 }]); // END
+
+trvlApp.controller('citySearchCtrl', ["$scope", "constants", function($scope, constants) {
+  $scope.test = 'Two way directive pass';
+
+  // set bounds of search to the whole world
+  var bounds = new google.maps.LatLngBounds(
+    new google.maps.LatLng(-90, -180),
+    new google.maps.LatLng(90, 180)
+  );
+
+  // get place search input element (only one on page at a time, because ID)
+  var input = document.getElementById('place-search-input');
+
+  // create options object
+  var options = {
+    bounds: bounds,
+    types: ['(cities)']
+  };
+
+  // new autocomplete object that will actually initialize autocomplete
+  var autocomplete = new google.maps.places.Autocomplete(input, options);
+
+  // function to log place data
+  var getPlaceDetails = function() {
+    // raw place data from autocomplete, returned after city selected
+    var rawPlaceData = autocomplete.getPlace();
+
+    $scope.cityData = {}; // create city data object
+
+    // add to cityData object from raw place data
+    $scope.cityData.geo = getAddressComponents(rawPlaceData.address_components);
+    $scope.cityData.placeString = rawPlaceData.formatted_address;
+    $scope.cityData.placeId = rawPlaceData.place_id;
+    $scope.cityData.geo.lat = rawPlaceData.geometry.location.lat();
+    $scope.cityData.geo.lng = rawPlaceData.geometry.location.lng();
+    $scope.cityData.iconUrl = rawPlaceData.icon;
+    $scope.cityData.mapUrl = rawPlaceData.url;
+    $scope.$apply(); // update scope
+    console.log('Place data pulled from google API');
+  };
+
+  // function to get correct address components from address array
+  var getAddressComponents = function(addCompArr) {
+    var finalComps = {};
+    var len = addCompArr.length;
+    // for all address components in array
+    finalComps.city = addCompArr[0].long_name; // always first
+    finalComps.region = addCompArr[len - 2].long_name; // always second to last
+    finalComps.country = addCompArr[len - 1].long_name; // always last
+
+    return finalComps;
+  };
+
+  // when new place is selected, log results obj of place
+  autocomplete.addListener('place_changed', getPlaceDetails);
+
+}]);
+
+trvlApp.directive('citySearch', function() {
+  return {
+    templateUrl: 'app/directives/citySearch/citySearchTmpl.html',
+    restrict: 'E',
+    scope: {
+      cityChosen: '=',
+      cityData: '='
+    },
+    controller: 'citySearchCtrl'
+  };
+});
+
+trvlApp.controller('menuBarCtrl', ["$scope", "authSvc", function($scope, authSvc) {
+
+  $scope.logout = function() {
+    authSvc.signOut();
+  };
+
+
+}]);
+
+trvlApp.directive('menuBar', function() {
+  return {
+    templateUrl: 'app/directives/menuBar/menuBarTmpl.html',
+    restrict: 'E',
+    scope: {
+      userData: '=',
+      currData: '='
+    },
+    controller: 'menuBarCtrl'
+  };
+});
